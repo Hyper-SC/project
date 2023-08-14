2176e15b1fa0fab7ed6c8b8bb72dad749486b977
zarshed511 <zarshedkarimkulov77@gamil.com>
2023-08-14 11:59:57 +0500

    first commit

---
+++ js/main.js
@@ -0,0 +1,63 @@
+// 1 misol
+// let a = 7
+// let c = 4*a
+
+// console.log(c);
+
+
+
+
+
+// 2 misol
+
+// const b = prompt("C Soni kirit")
+// const a = prompt("M soni kirit")
+// if(b<a){
+//     console.log("Katta " + a)
+// } 
+// else{
+//     console.log("Katta " + b)
+// }
+
+
+
+
+
+
+// 3 misol
+// let d = prompt("haftani sonini kirit")
+
+//     if (d==1) {
+//         console.log("dushanba");
+//     } else  if (d==2) {
+//         console.log("seshanba");
+//     } else  if (d==3) {
+//         console.log("chorshanba");
+//     }  else if (d==4) {
+//         console.log("payshanba");
+//     }   else if (d==5) {
+//         console.log("juma")
+//     }  else  if (d==6) {
+//         console.log("shanba");
+//     }   else if (d==7) {
+//         console.log("yakshanba");
+//     }
+//     else {
+//         console.log("bunaqa kuni yoq");
+//     }
+
+
+
+
+// 4 misol
+
+let n = 10
+let k = 8
+for(let i = 0; i < n; i++){
+    console.log(k);
+}
+
+
+
+// 5 misol
+
